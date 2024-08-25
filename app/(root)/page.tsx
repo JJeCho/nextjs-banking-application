43a506b54d5e2356a97f5aa67b9a21fcd3e26715
JJeCho <james.je.cho@gmail.com>
2024-08-25 14:35:35 -0700

    working on sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,23 @@
-
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 const Home = () => {
+  const loggedIn = { firstName:'James'}
   return (
-    <div>page</div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+          type="greeting"
+          title="Welcome"
+          user={loggedIn?.firstName || "Guest"}
+          subtext="Access and manage your account"/>
+          <TotalBalanceBox
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={1250.78}/>
+        </header>
+      </div>
+    </section>
   )
 }
 
